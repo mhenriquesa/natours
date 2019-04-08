43e9ef5f14eba0feca3a4bb28591c1a2b31860b1
Henrique <henriqueator@gmail.com>
2019-04-08 01:43:30 -0300

    Configurado e atualizado Babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,19 @@ module.exports = {
 		path: path.resolve(__dirname, "./app/temp/scripts"),
 		filename: "App.js"
 	},
+	module: {
+	    rules: [
+	      {
+	        test: /\.js$/,
+	        exclude: /node_modules/,
+	        use: {
+	          loader: 'babel-loader',
+	          options: {
+	            presets: ['@babel/preset-env']
+	          }
+	        }
+	      }
+	    ]
+	  },
 	mode: "production"
 }
